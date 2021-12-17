f5790fe90649473519dce0e214d5cb3252267c92
Chiwai Yau <chiwai.yau@gravitysupplychain.com>
2021-12-17 10:00:18 +0800

    Updated chinese-character-helper

--- chinese-character-helper.js
+++ chinese-character-helper.js
@@ -1,9 +1,13 @@
 const characterToChangJieLookUpData = require('./data/characterToChangjieLookUp.json');
 const characterToCantonesePinyinLookUp = require('./data/characterToCantonesePinyinLookUp.json');
 const cantonesePinyinToCharacterLookUp = require('./data/cantonesePinyinToCharacterLookUp.json');
+
 const ChineseCharacterHelper = {
   isChineseCharacter: (str) => {
     return /^[\u4e00-\u9fa5]+$/.test(str);
+  },
+  containsChineseCharacter: (str) => {
+    return /[\u4E00-\u9FA5]/.test(str);
   },
   getChangJieByCharacter: (character) => {
     if (ChineseCharacterHelper.isChineseCharacter(character)) {
@@ -14,6 +18,24 @@ const ChineseCharacterHelper = {
       }
     }
     return character;
+  },
+  getChangJieByCharacters: function (str) {
+    const characters = [];
+    for (let i = 0; i < str.length; i++) {
+      const character = str[i];
+      if (ChineseCharacterHelper.isChineseCharacter(character)) {
+        const changJieCharacter =
+          ChineseCharacterHelper.getChangJieByCharacter(character);
+        if (changJieCharacter !== undefined) {
+          characters.push(changJieCharacter + ' ');
+        } else {
+          characters.push(character + ' ');
+        }
+      } else {
+        characters.push(character + ' ');
+      }
+    }
+    return characters;
   },
   getCharacterByCantonesePinyin: (pinyin) => {
     return cantonesePinyinToCharacterLookUp[pinyin];
@@ -27,6 +49,27 @@ const ChineseCharacterHelper = {
       }
     }
     return character;
+  },
+  getCantonesePinyinByCharacters: (str) => {
+    const pinyin = [];
+    for (let i = 0; i < str.length; i++) {
+      const character = str[i];
+      if (ChineseCharacterHelper.isChineseCharacter(character)) {
+        const pinyinOfCharacter =
+          ChineseCharacterHelper.getCantonesePinyinByCharacter(character);
+        if (pinyinOfCharacter !== undefined) {
+          pinyin.push(pinyinOfCharacter);
+        } else {
+          pinyin.push(character);
+        }
+      } else {
+        pinyin.push(character);
+      }
+    }
+    return pinyin;
+  },
+  formatResult: (result, joinCharacter) => {
+    return result.join(joinCharacter);
   },
 };
 
